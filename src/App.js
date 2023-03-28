353d5eaa7ba48b736d7c350af59c09602e10623b
Arunkumar S <70270364+Arunaks007@users.noreply.github.com>
2023-03-28 19:25:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,159 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+import colorData from "../src/color.json";
+import Grid from "./componets/Grid";
+
+export default function App() {
+  const [data, setData] = useState([
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""]
+  ]);
+  const [scores, setScores] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPressed);
+    return () => {
+      document.removeEventListener("keydown", keyPressed);
+    };
+  }, []);
+
+  const keyPressed = (event) => {
+    if (event.key.includes("Arrow")) {
+      switch (event.key.toLowerCase()) {
+        case "arrowup":
+          moveTiles("up");
+          break;
+        case "arrowdown":
+          moveTiles("down");
+          break;
+        case "arrowleft":
+          moveTiles("left");
+          break;
+        case "arrowright":
+          moveTiles("right");
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  const applyColor = (number) => {
+    return number !== "" ? { backgroundColor: colorData[number] } : {};
+  };
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 4);
+  };
+
+  const moveTiles = (direction) => {
+    if (direction === "down") {
+      for (let i = 0; i < data.length - 1; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+          // checks if the value is not empty
+          if (data[i + 1][j] === "") {
+            data[i + 1][j] = data[i][j];
+            data[i][j] = "";
+          }
+          // checks if the down position is empty then swap the values
+          else if (data[i + 1][j] === data[i][j]) {
+            // checks if down position value is same as current then assign multiple of current value to the down position
+            data[i + 1][j] = data[i][j] * 2;
+            data[i][j] = "";
+            setScores((_) => _ + Number(data[i + 1][j]));
+          }
+        }
+      }
+    } else if (direction === "up") {
+      for (let i = data.length - 1; i > 0; i--) {
+        for (let j = 0; j < data[i].length; j++) {
+          if (data[i - 1][j] === "") {
+            data[i - 1][j] = data[i][j];
+            data[i][j] = "";
+          } else if (data[i - 1][j] === data[i][j]) {
+            data[i - 1][j] = data[i][j] * 2;
+            data[i][j] = "";
+            setScores((_) => _ + Number(data[i - 1][j]));
+          }
+        }
+      }
+    } else if (direction === "right") {
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length - 1; j++) {
+          if (data[i][j + 1] === "") {
+            data[i][j + 1] = data[i][j];
+            data[i][j] = "";
+          } else if (data[i][j + 1] === data[i][j]) {
+            data[i][j + 1] = data[i][j] * 2;
+            data[i][j] = "";
+            setScores((_) => _ + Number(data[i][j + 1]));
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < data.length; i++) {
+        for (let j = data[i].length - 1; j > 0; j--) {
+          if (data[i][j - 1] === "") {
+            data[i][j - 1] = data[i][j];
+            data[i][j] = "";
+          } else if (data[i][j - 1] === data[i][j]) {
+            data[i][j - 1] = data[i][j] * 2;
+            data[i][j] = "";
+            setScores((_) => _ + Number(data[i][j - 1]));
+          }
+        }
+      }
+    }
+    setData([...data]);
+    addNewTile();
+  };
+
+  const addNewTile = () => {
+    let row;
+    let col;
+
+    try {
+      do {
+        row = getRandomNumber();
+        col = getRandomNumber();
+        if (data[row][col] === "") {
+          data[row][col] = "2";
+          break;
+        }
+      } while (true);
+    } catch (Exc) {
+      setGameOver(true);
+    }
+
+    setData([...data]);
+    document.getElementsByName(row)[col].className = "titleNew";
+  };
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>2048</h1>
+        <div className="scores">
+          <h2>SCORE</h2>
+          <h3>{scores}</h3>
+        </div>
+      </div>
+      <Grid
+        data={data}
+        setData={setData}
+        applyColor={applyColor}
+        getRandomNumber={getRandomNumber}
+        gameOver={gameOver}
+        moveTiles={moveTiles}
+      />
+      <div className="mobile">
+        <p>Use your fingers to swipe</p>
+      </div>
+    </div>
+  );
+}
+
+// Swipe Up / Down / Left / Right
---
+++ src/componets/Grid.js
@@ -0,0 +1,111 @@
+import { useEffect, useState, useRef } from "react";
+
+export default function Grid({
+  applyColor,
+  data,
+  getRandomNumber,
+  setData,
+  gameOver,
+  moveTiles
+}) {
+  const [touchStart, setTouchStart] = useState({ x: 0, y: 0 });
+  const [touchEnd, setTouchEnd] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    assignNewValueOnFirst();
+    document.addEventListener(
+      "touchmove",
+      function (event) {
+        event.preventDefault();
+      },
+      { passive: false }
+    );
+  }, []);
+
+  const handleTouchStart = (e) => {
+    e.preventDefault();
+    setTouchStart({ x: e.touches[0].clientX, y: e.touches[0].clientY });
+  };
+
+  const handleTouchMove = (e) => {
+    e.preventDefault();
+    setTouchEnd({ x: e.touches[0].clientX, y: e.touches[0].clientY });
+  };
+
+  const handleTouchEnd = (e) => {
+    const { x: startX, y: startY } = touchStart;
+    const { x: endX, y: endY } = touchEnd;
+
+    const dx = endX - startX;
+    const dy = endY - startY;
+
+    const angle = (Math.atan2(dy, dx) * 180) / Math.PI;
+    const swipeDistance = Math.sqrt(dx * dx + dy * dy);
+
+    if (swipeDistance > 50) {
+      if (angle >= -45 && angle < 45) {
+        // Swipe right
+        moveTiles("right");
+      } else if (angle >= 45 && angle < 135) {
+        // Swipe up
+
+        moveTiles("down");
+        e.preventDefault();
+      } else if (angle >= -135 && angle < -45) {
+        // Swipe down
+        moveTiles("up");
+      } else {
+        // Swipe left
+        moveTiles("left");
+      }
+    }
+  };
+
+  function assignNewValueOnFirst() {
+    let row1 = getRandomNumber();
+    let column1 = getRandomNumber();
+
+    let row2 = getRandomNumber();
+    let column2 = getRandomNumber();
+
+    data[row1][column1] = "2";
+    data[row2][column2] = "2";
+
+    setData([...data]);
+  }
+
+  return (
+    <>
+      <div
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+        className="grid"
+      >
+        {data.map((row, key1) => {
+          return row.map((col, key2) => {
+            return (
+              <button
+                name={key1}
+                index={String(key1) + key2}
+                style={applyColor(col)}
+              >
+                {col}
+              </button>
+            );
+          });
+        })}
+        <div className={gameOver ? "gameover" : "game"}>
+          <h1>Game Over</h1>
+          <button
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            Try Again
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
